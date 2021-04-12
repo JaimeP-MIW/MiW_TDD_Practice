6be7b122b63747100ea51a7717de1a18ecd5cb02
Jaime Pérez <jaime.perez@alumnos.upm.es>
2021-04-12 17:13:30 +0200

    green

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -16,7 +16,7 @@ public class Interval {
 	}
 
 	public boolean intersects(Interval interval) {
-		return this.max.isWithin(interval.getMinValue()) && 
+		return this.max.isWithin(interval.getMinValue()) && interval.min.isWithin(this.max.getValue()) &&
 				this.min.isWithin(interval.getMaxValue());
 	}
 
